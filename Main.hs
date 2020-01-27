051e9ea8e843522d88c7737ab545deafc05104ce
jacob hazen <37519173+JacobHazen1@users.noreply.github.com>
2020-01-27 16:42:22 -0700

    Add files via upload

---
+++ Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Checkers
+import Moves
+
+
+main :: IO ()
+
+main = human apply_move initialGameState
+
+-- main = human apply_move g7
+
+ 
+-- main = redAi (ai_move_red 10) apply_move initialGameState
+-- main = blackAi (ai_move_black 10) apply_move initialGameState
+--main = aiTest (ai_move_red 10)  (ai_move_black 10) apply_move initialGameState
+
+---------------------------------------------------------
+--                       YOUR CODE GOES HERE!!!!
+---------------------------------------------------------
+--   define apply_move and supporting functions here (or make a module)
+
